a1821711c2e40dff734e1bc13c0531d08015eac8
sajonasJ <jonas.sajonas@gmail.com>
2024-09-28 19:11:45 +1000

    added footer and header layout, and image header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,9 @@
 <main>
-    header
+    <div id="courseBanner" class="course-banner">
+        <div class="course-banner__text">
+            <p class="course-banner__code">3813ICT</p>
+            <h2 class="course-banner__title">Software Frameworks</h2>
+        </div>
+        <img class="course-banner__image" src="{{ asset('griffith.jpg') }}" alt="Course Banner Image">
+    </div>
 </main>
