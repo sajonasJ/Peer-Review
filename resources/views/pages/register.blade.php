1ece37d0347fd2dca00088a699b095a38e07112e
JonasJ <117071036+sajonasJ@users.noreply.github.com>
2024-10-03 19:33:29 +1000

    refactored register page

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -21,29 +21,23 @@
                                 <label for="name">Full Name</label>
                                 <input type="text" name="name" class="form-control" placeholder="Enter your name">
                             </div>
-
                             <div class="form-group mb-3">
                                 <label for="email">Email Address</label>
                                 <input type="email" name="email" class="form-control" placeholder="Enter your email">
                             </div>
-
                             <div class="form-group mb-3">
                                 <label for="snumber">sNumber</label>
                                 <input type="text" name="snumber" class="form-control" placeholder="Enter your sNumber">
                             </div>
-
                             <div class="form-group mb-3">
                                 <label for="password">Password</label>
                                 <input type="password" name="password" class="form-control" placeholder="Create a password">
                             </div>
-
                             <div class="form-group mb-3">
                                 <label for="password_confirmation">Confirm Password</label>
                                 <input type="password" name="password_confirmation" class="form-control"
                                     placeholder="Re-type password">
                             </div>
-
-                            <!-- Display Validation Errors -->
                             @if ($errors->any())
                                 <div class="alert alert-danger">
                                     <ul>
@@ -53,18 +47,22 @@
                                     </ul>
                                 </div>
                             @endif
-
                             <div class="form-group text-center">
                                 <button type="submit" class="btn btn-danger btn-block">Register</button>
                             </div>
                         </form>
                     </div>
                 </div>
-                <!-- Back to login link -->
                 <div class="text-center mt-3">
                     <p>Already have an account? <a href="/"
-                            class="text-danger link-offset-0 link-offset-1-hover link-danger link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Back
-                            to Login</a></p>
+                            class="text-danger 
+                            link-offset-0 
+                            link-offset-1-hover 
+                            link-danger 
+                            link-underline 
+                            link-underline-opacity-0 
+                            link-underline-opacity-75-hover">
+                            Backto Login</a></p>
                 </div>
             </div>
         </div>
