9f45ab3f2719dc17abb987212697432203cc059f
JonasJ <117071036+sajonasJ@users.noreply.github.com>
2024-10-03 19:28:51 +1000

    refactored view-assessments page

--- resources/views/pages/view-assessments.blade.php
+++ resources/views/pages/view-assessments.blade.php
@@ -15,7 +15,9 @@
         <div class="course-title px-3 py-2">
             <div class="d-flex align-items-center justify-content-between">
                 <div class="d-flex gap-4">
-                    <a href="{{ route('course-details', ['courseCode' => $course->course_code]) }}"
+                    <a href="{{ route('course-details', [
+                        'courseCode' => $course->course_code,
+                    ]) }}"
                         class="btn btn-sm h-25 btn-warning">Back</a>
                     <h3>{{ $course->course_code }} {{ $course->name }}</h3>
                 </div>
@@ -34,8 +36,10 @@
                         <p><strong>Instructions:</strong> {{ $assessment->instruction }}</p>
                         <p><strong>Number of Reviews:</strong> {{ $assessment->num_reviews }}</p>
                         <p><strong>Maximum Score:</strong> {{ $assessment->max_score }}</p>
-                        <p><strong>Due Date:</strong> {{ \Carbon\Carbon::parse($assessment->due_date)->format('d F, Y') }}</p>
-                        <p><strong>Due Time:</strong> {{ \Carbon\Carbon::parse($assessment->due_time)->format('h:i A') }}</p>
+                        <p><strong>Due Date:</strong> {{ \Carbon\Carbon::parse($assessment->due_date)->format('d F, Y') }}
+                        </p>
+                        <p><strong>Due Time:</strong> {{ \Carbon\Carbon::parse($assessment->due_time)->format('h:i A') }}
+                        </p>
                         <p><strong>Review Type:</strong> {{ $assessment->type }}</p>
                     </div>
                 </div>
@@ -46,27 +50,26 @@
                         <h4>Your Reviews Sent for This Assessment</h4>
                     </div>
                     <div class="card-body p-0">
-                        @if($reviewsSent->isEmpty())
+                        @if ($reviewsSent->isEmpty())
                             <p class="p-2">You have not submitted any reviews for this assessment yet.</p>
                         @else
-                        <ul class="list-group">
-                            @foreach ($reviewsSent as $review)
-                                <li class="list-group-item">
-                                    <p><strong>Review for:</strong> {{ $review->reviewee->name }}</p>
-                                    <p><strong>Rating:</strong> 
-                                        @for ($i = 1; $i <= 5; $i++)
-                                            @if ($i <= $review->rating)
-                                                <i class="bi bi-star-fill text-warning"></i> <!-- Filled star for rating -->
-                                            @else
-                                                <i class="bi bi-star text-muted"></i> <!-- Unfilled star for the rest -->
-                                            @endif
-                                        @endfor
-                                    </p>
-                                    <p><strong>Comments:</strong> {{ $review->review_text }}</p>
-                                </li>
-                            @endforeach
-                        </ul>
-                        
+                            <ul class="list-group">
+                                @foreach ($reviewsSent as $review)
+                                    <li class="list-group-item">
+                                        <p><strong>Review for:</strong> {{ $review->reviewee->name }}</p>
+                                        <p><strong>Rating:</strong>
+                                            @for ($i = 1; $i <= 5; $i++)
+                                                @if ($i <= $review->rating)
+                                                    <i class="bi bi-star-fill text-warning"></i>
+                                                @else
+                                                    <i class="bi bi-star text-muted"></i>
+                                                @endif
+                                            @endfor
+                                        </p>
+                                        <p><strong>Comments:</strong> {{ $review->review_text }}</p>
+                                    </li>
+                                @endforeach
+                            </ul>
                         @endif
                     </div>
                 </div>
@@ -77,27 +80,27 @@
                         <h4>Reviews You Have Received for This Assessment</h4>
                     </div>
                     <div class="card-body p-0">
-                        @if($reviewsReceived->isEmpty())
+                        @if ($reviewsReceived->isEmpty())
                             <p class="p-2">No reviews have been submitted for you yet.</p>
                         @else
-                        <ul class="list-group">
-                            @foreach ($reviewsReceived as $review)
-                                <li class="list-group-item">
-                                    <p><strong>Reviewer:</strong> {{ $review->reviewer->name }}</p>
-                                    <p><strong>Rating:</strong> 
-                                        @for ($i = 1; $i <= 5; $i++)
-                                            @if ($i <= $review->rating)
-                                                <i class="bi bi-star-fill text-warning"></i> <!-- Filled star for rating -->
-                                            @else
-                                                <i class="bi bi-star text-muted"></i> <!-- Unfilled star for rest -->
-                                            @endif
-                                        @endfor
-                                    </p>
-                                    <p><strong>Comments:</strong> {{ $review->review_text }}</p>
-                                </li>
-                            @endforeach
-                        </ul>
-                        
+                            <ul class="list-group">
+                                @foreach ($reviewsReceived as $review)
+                                    <li class="list-group-item">
+                                        <p><strong>Reviewer:</strong> {{ $review->reviewer->name }}</p>
+                                        <p><strong>Rating:</strong>
+                                            @for ($i = 1; $i <= 5; $i++)
+                                                @if ($i <= $review->rating)
+                                                    <i class="bi bi-star-fill text-warning"></i>
+                                                @else
+                                                    <i class="bi bi-star text-muted"></i>
+                                                @endif
+                                            @endfor
+                                        </p>
+                                        <p><strong>Comments:</strong> {{ $review->review_text }}</p>
+                                    </li>
+                                @endforeach
+                            </ul>
+
                         @endif
                     </div>
                 </div>
@@ -113,31 +116,31 @@
                                 placeholder="Search student by name or sNumber">
                         </div>
                         <div class="card-body p-0">
-                        
-                                <ul class="list-group p-0">
-                                    @foreach ($students as $student)
-                                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                                            <div>
-                                                <p><strong>Name:</strong> {{ $student->name }}</p>
-                                                <p><strong>Student Number:</strong> {{ $student->snumber }}</p>
-                                            </div>
-                                            @if (Auth::guard('web')->check())
-                                                <!-- Button for Add Peer Review -->
-                                                <a href="{{ route('add-review', [
-                                                    'courseCode' => $course->course_code,
-                                                    'studentId' => $student->id,
-                                                    'assessmentId' => $assessment->id,
-                                                ]) }}"
-                                                    class="btn btn-primary btn-sm">Add Peer Review</a>
-                                            @endif
-                                        </li>
-                                    @endforeach
-                                </ul>
-                                <!-- Pagination Links -->
-                                <div class="mt-3 d-flex justify-content-center">
-                                    {!! $students->appends(['showStudents' => request('showStudents')])->links('pagination::bootstrap-4') !!}
-                                </div>
-                         
+
+                            <ul class="list-group p-0">
+                                @foreach ($students as $student)
+                                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                                        <div>
+                                            <p><strong>Name:</strong> {{ $student->name }}</p>
+                                            <p><strong>Student Number:</strong> {{ $student->snumber }}</p>
+                                        </div>
+                                        @if (Auth::guard('web')->check())
+                                            <a href="{{ route('add-review', [
+                                                'courseCode' => $course->course_code,
+                                                'studentId' => $student->id,
+                                                'assessmentId' => $assessment->id,
+                                            ]) }}"
+                                                class="btn btn-primary btn-sm">Add Peer Review</a>
+                                        @endif
+                                    </li>
+                                @endforeach
+                            </ul>
+                            <!-- Pagination Links -->
+                            <div class="mt-3 d-flex justify-content-center">
+                                {!! $students->appends([
+                                        'showStudents' => request('showStudents'),
+                                    ])->links('pagination::bootstrap-4') !!}
+                            </div>
                         </div>
                     </div>
                 </div>
